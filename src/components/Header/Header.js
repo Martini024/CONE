50d66e7b87f13466f3adf355411561b082cff5bd
martini <zhao435021640@gmail.com>
2019-04-10 17:10:25 +0800

    v0.1.1 almost finish navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,71 @@
 import React, {Component} from 'react';
 import './Header.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Headroom from 'react-headroom';
 
 class Header extends Component {
     render() {
         return (
-            <div className="navbar">
-                Navbar
-            </div>
+            <Headroom>
+                <header className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-2">
+                                <a>
+                                    <img className="logo" alt="" src={require('./logo.png')} width="1000px" height="400px"></img>
+                                </a>
+                            </div>
+
+                            <div className="col-8">
+                                <ul className="nav justify-content-around">
+                                    <li className="nav-item">
+                                        About Us
+                                    </li>
+                                    <li className="nav-item">
+                                        Events
+                                    </li>
+                                    <li className="nav-item">
+                                        Projects
+                                    </li>
+                                    <li className="nav-item">
+                                        Get Involved
+                                    </li>
+                                    <li className="nav-item">
+                                        <a>
+                                            <FontAwesomeIcon icon="search"/>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="col-2">
+                                <ul className="nav justify-content-end">
+                                    <li className="nav-item">
+                                        <a>
+                                            <FontAwesomeIcon icon={["fab", "facebook"]}/>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a>
+                                            <FontAwesomeIcon icon={["fab", "twitter"]}/>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a>
+                                            <FontAwesomeIcon icon={["fab", "youtube"]}/>
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a>
+                                            <FontAwesomeIcon icon={["fab", "instagram"]}/>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </Headroom>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import Home from './scenes/Home/Home';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab, faSearch);
 
 ReactDOM.render((
     <Home />
